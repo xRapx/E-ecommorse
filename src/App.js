c54e08c2b9bddb08a5d09bc0aa02b8c0cb0e6267
xRapx <102133684+xRapx@users.noreply.github.com>
2024-03-06 11:14:47 +0700

    #10 - Create Layout Router & Header Component & NavBar-Link

--- src/App.js
+++ src/App.js
@@ -1,9 +1,58 @@
-import './App.css';
+/* eslint-disable no-lone-blocks */
+
+import {Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from 'react-router-dom'
+import {ToastContainer} from 'react-toastify'
+import "react-toastify/dist/ReactToastify.css";
+
+import Home from './pages/Home';
+import Shop from './pages/Shop';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Header from './component/DefaultLayout/Header';
+
+{/*====================Layout Component==================== */}
+const Layout = () =>{
+  return(
+    <div>
+        <ToastContainer
+            position="top-right"
+            autoClose={1000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="colored"
+        />
+        <Header />
+    </div>
+  )
+}
+
+ {/*====================Value Props Routers==================== */}
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+        <Route path="/" element={<Layout />} >
+          {/*====================Children Layout==================== */}
+          <Route index element={<Home />}></Route>
+          <Route path="/shop" element={<Shop />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+
+        </Route>
+    </Route>
+  )
+)
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1>E-ecommorse WEB</h1>
+        <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/contans/index.js
@@ -0,0 +1,26 @@
+
+
+// =================== NavBarList Start here ====================
+export const navBarList = [
+	{
+	  _id: 1001,
+	  title: "Home",
+	  link: "/",
+	},
+	{
+	  _id: 1002,
+	  title: "Shop",
+	  link: "/shop",
+	},
+	{
+	  _id: 1003,
+	  title: "About",
+	  link: "/about",
+	},
+	{
+	  _id: 1004,
+	  title: "Contact",
+	  link: "contact",
+	},
+  ];
+  // =================== NavBarList End here ======================
